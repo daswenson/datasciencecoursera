60ececb3c0e0536dc2d28be04631f98725a1deb9
greendud370 <swenso7@stolaf.edu>
2020-12-26 12:18:42 -0600

    Part 2 of assignment 1

---
+++ R Programming/complete.R
@@ -0,0 +1,26 @@
+# Part 2
+# Write a function that reads a directory full of files and reports the number
+#of completely observed cases in each data file. The function should return a
+#data frame where the first column is the name of the file and the second
+#column is the number of complete cases. 
+
+complete <- function(directory,id = 1:332){
+  
+  #lists for id column and observation column
+  row_names <- c()
+  case_list <- c()
+  
+  #going through each file and appending id # and obs count
+  for(i in id){
+    pre <- if(i<10){"00"}
+    else if(i<100){"0"}
+    else {next}
+    file_name <- paste0(directory,"/",pre,toString(i),".csv")
+    row_names <- append(row_names,i)
+    data <- read.csv(file = file_name)
+    case_list <- append(case_list,nrow(na.omit(data)))
+  }
+  
+  #making data frame with the two lists
+  data.frame(id = row_names,nobs = case_list)
+}
