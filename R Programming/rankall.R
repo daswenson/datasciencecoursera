ec644f600f418af1e5bb3217b64aaea97efdbb6a
daswenson <daswenson7@gmail.com>
2021-01-06 17:30:12 -0600

    moving notes and other minor changes

--- R Programming/rankall.R
+++ R Programming/rankall.R
@@ -25,8 +25,8 @@ rankall <- function(outcome, num = "best") {
   state <- unique(rank_data$state[order(rank_data$state)])
   
   # setting the wanted rank to the required integer
-  for(state in state_list){
-    bool <- which(rank_data$state == state) # getting bool for rows of state
+  for(st in state){
+    bool <- which(rank_data$state == st) # getting bool for rows of state
     state_rows <- rank_data[bool,] # get rows of state using boolean
     state_rows[,outcome] <- suppressWarnings(as.numeric(state_rows[,outcome]))
     
@@ -37,24 +37,24 @@ rankall <- function(outcome, num = "best") {
       if(num == "worst"){
         outcome_order <- state_rows[
           order(state_rows[,outcome],state_rows$hospital, decreasing = TRUE),]
-        hospital_col <- append(hospital_col,outcome_order$hospital[1])
+        hospital <- append(hospital,outcome_order$hospital[1])
         
       } else if (num == "best"){
         num <-1
         outcome_order <- state_rows[
           order(state_rows[,outcome],state_rows$hospital),]
-        hospital_col <- append(hospital_col,outcome_order$hospital[num])
+        hospital <- append(hospital,outcome_order$hospital[num])
       }
       
     } else {
       outcome_order <- state_rows[
         order(state_rows[,outcome],state_rows$hospital),]
-      hospital_col <- append(hospital_col,outcome_order$hospital[num])
+      hospital <- append(hospital,outcome_order$hospital[num])
     }
   }
   
   col_names = c("hospital", "state")
-  output <- as.data.frame(cbind(hospital_col,state_list),
+  output <- as.data.frame(cbind(hospital,state),
                           col.names = "hospital", "state")
   rownames(output) <- output[,2]
   return(output)
