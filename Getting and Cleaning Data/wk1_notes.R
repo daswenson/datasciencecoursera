022d62261eb07bad3a6837e4912e9d1eda8bd25b
daswenson <daswenson7@gmail.com>
2021-01-07 19:29:31 -0600

    added data table notes

--- Getting and Cleaning Data/wk1_notes.R
+++ Getting and Cleaning Data/wk1_notes.R
@@ -98,3 +98,67 @@ myjson
     # turning it back into data frame
 iris2 <- fromJSON(myjson)
 head(iris2)
+
+  ## data tables
+    # built using C so is much faster
+    # requires some C syntax
+
+library(data.table)
+DF = data.frame(x=rnorm(9),y=rep(c("a","b","c"),each=3),z=rnorm(9))
+head(DF,3)
+
+DT = data.table(x=rnorm(9), y=rep(c("a","b","c"),each=3),z=rnorm(9))
+head(DT,3)
+
+# see all data tables in memory
+tables()
+
+    # subsetting rows is just like a data frame
+DT[2,]
+DT[DT$y=="a",]
+    # subsetting col is differenet
+    # argument after comma is an "expression"
+DT[,c(2,3)] # getting columns 2 and 3
+DT[,list(mean(x),sum(z))] # get mean of x and sum of z
+
+    # adding new columns
+    # adding a new variable to a data frame R copies everything
+      # thus takes a lot of memory
+DT[,w:=z^2] # adds column w that is z^2
+
+DT2 <- DT
+DT[,y:=2]
+head(DT,3)
+head(DT2,3) # since no copy was made, both were changed, uses copy()
+
+    # able to use multiple expressions, last thing gets returned 
+DT[,m:= {tmp<- (x+z); log2(tmp+5)}]
+DT[,a:=x>0] # can also do plyr operations
+DT[,b:= mean(x+w), by=a] # groups by when a=TRUE or a=FALSE
+
+    # special variables are present
+      # .N - integer containing the number of times something appears
+set.seed(123)
+DT <- data.table(x=sample(letters[1:3], 1E5,TRUE))
+DT[,.N, by=x]
+    
+    # keys
+      # possible subset and sort much faster
+      # use them to faciliate joins
+DT <- data.table(x=rep(c("a","b","c"), each=100), y=rnorm(300))
+setkey(DT,x)
+DT['a'] # thus subsets on the basis x
+
+DT1 <- data.table(x=c('a','a','b','dt1'),y=1:4)
+DT2 <- data.table(x=c('a','b','dt2'), z=5:7)
+setkey(DT1,x); setkey(DT2,x) # as long as same key
+merge(DT1,DT2)
+
+    # fast reading
+big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
+file <- tempfile()
+write.table(big_df, file=file, row.names=FALSE,
+            col.names=TRUE, sep="\t", quote=FALSE)
+# compare the two
+system.time(fread(file))
+system.time(read.table(file, header = TRUE, sep = "\t"))
